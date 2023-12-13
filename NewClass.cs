9921b2e6fb1cc1032597b177b523960d7b95d616
Andrey Gulenko <consul-guard@proton.me>
2023-12-13 13:19:16 +0300

    Изменили название команды разраб на 2

--- NewClass.cs
+++ NewClass.cs
@@ -11,7 +11,7 @@ namespace ConsoleHome
 
         public NewClass() 
         {
-            Console.WriteLine("Команда разработчиков 1");
+            Console.WriteLine("Команда разработчиков 2");
         }
     }
 }
